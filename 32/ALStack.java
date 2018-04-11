099e3979dbe45128ee343856ed89af8d67b5b94e
Lily Yan <lyan@stuy.edu>
2018-04-11 08:39:09 -0400

    hw32

---
+++ 32/ALStack.java
@@ -0,0 +1,13 @@
+public class ALStack<PANCAKE> implements Stack<PANCAKE> {
+    public boolean isEmpty() {
+    }
+
+    public PANCAKE peek() {
+    }
+
+    public PANCAKE pop() {
+    }
+
+    public void push(PANCAKE x) {
+    }
+}
---
+++ 32/Stckr.java
@@ -0,0 +1,16 @@
+/*****************************************************
+ * class Stckr
+* driver/tester for Stack implementations (Linked-list-based, ArrayList-based)
+ *****************************************************/
+
+public class Stckr 
+{
+public static void main( String[] args )
+{
+//Stack cakes = new ALStack()...
+//Stack cakes = new LLStack()...
+
+}
+
+}//end class
+
