e013426eaadb8013e92e6e08eeb4ae1472a56db5
Lily Yan <lyan@stuy.edu>
2018-04-11 08:41:41 -0400

    hw32

--- 32/LLStack.java
+++ 32/LLStack.java
@@ -1,8 +1,19 @@
+import java.util.LinkedList;
+
 public class LLStack<PANCAKE> implements Stack<PANCAKE> {
+
+    private LinkedList _stack;
+
+    public LLStack {
+	_stack = new LinkedList();
+
+    }
+
     public boolean isEmpty() {
     }
 
     public PANCAKE peek() {
+	
     }
 
     public PANCAKE pop() {
