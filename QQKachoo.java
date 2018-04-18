cf2555480586f4248edfd4dee247418ec3086d64
LilyYan2001 <lyan@stuy.edu>
2018-04-18 18:26:34 -0400

    hw37

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,7 +1,7 @@
 /*
-  Team Yahoo Search -- Bing Li, Lily Yan
+  Team Yahoo Search -- Bing Li, Lily Yan, Selina Zou
   APCS2 pd1
-  HW37
+  Lab 02
   2018-04-18
 */
 
@@ -14,16 +14,16 @@ public class QQKachoo<T> implements Deque<T> {
 
     //constructor
     public QQKachoo() {
-	_front = new DLLNode<T>(null, null, null);
-	_end = new DLLNode<T>(null, _front, null);
-	_front.setNext(_end);
+	_front = new DLLNode<T>(null, null, null); 
+	_end = new DLLNode<T>(null, _front, null); 
+	_front.setNext(_end); //linking front and end to each other
 	_size = 0; //deque empty at birth
     }
 
     //peek methods
     public T peekFirst() {
 	if (_size == 0) return null;
-	return _front.getCargo();
+	return _front.getCargo(); 
     }
     
     public T peekLast() {
@@ -33,35 +33,36 @@ public class QQKachoo<T> implements Deque<T> {
 
     //poll methods
     public T pollFirst() {
-	if (_size == 0) return null;
-	T ans = _front.getCargo();
-	_front = _front.getNext();
-	_size -= 1;
+	if (_size == 0) return null; //if empty, return null
+	
+	T ans = _front.getCargo(); //store cargo of node to be removed
+	_front = _front.getNext(); //removing front node
+	_size -= 1; //decreasing size
 	return ans;
     }
 
     public T pollLast() {
 	if (_size == 0) return null;
 	T ans = _end.getCargo();
-	_end = _end.getPrev();
+	_end = _end.getPrev(); //removing end node
 	_size -= 1;
 	return ans;
     }
 
     //offer methods (Q: when do we return false?)
     public boolean offerFirst(T x) {
-        _front = new DLLNode<T>(x, null, _front);
+        _front = new DLLNode<T>(x, null, _front); //making node with x as cargo and former front node as its next node
 
 	if ( _size == 0 ) 
 	    _end = _front;
 	else 
 	    _front.getNext().setPrev( _front );
-	_size++;
+	_size++; //increment size
 	return true;
     }
 
     public boolean offerLast(T x) {
-        _end = new DLLNode<T>(x, _end, null);
+        _end = new DLLNode<T>(x, _end, null); //making node with x as cargo and former end node as its previous node
 
 	if ( _size == 0 ) 
 	    _front = _end;
@@ -71,7 +72,7 @@ public class QQKachoo<T> implements Deque<T> {
 	return true;
     }
 
-    //add methods
+    //add methods (do we need these?)
     public void addFirst(T x) {
 
     }
