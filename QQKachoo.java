54169517e4ca142311c7614f4d1ac8aae22a45c5
Lily Yan <lyan@stuy.edu>
2018-04-19 08:40:48 -0400

    finished coding?????

--- QQKachoo.java
+++ QQKachoo.java
@@ -11,14 +11,22 @@ public class QQKachoo<T> implements Deque<T> {
     //instance vars
     private DLLNode<T> _front, _end;
     public int _size;
+    private int _capacity;
 
-    //constructor
+    //default constructor
     public QQKachoo() {
 	_front = new DLLNode<T>(null, null, null); 
 	_end = new DLLNode<T>(null, _front, null); 
 	_front.setNext(_end); //linking front and end to each other
 	_size = 0; //deque empty at birth
+	_capacity = -1;
     }
+
+    public QQKachoo(int cap) {
+	this();
+	_capacity = cap;
+    }
+    
 
     //peek methods
     public T peekFirst() {
@@ -51,7 +59,10 @@ public class QQKachoo<T> implements Deque<T> {
 
     //offer methods (Q: when do we return false?)
     public boolean offerFirst(T x) {
-        _front = new DLLNode<T>(x, null, _front); //making node with x as cargo and former front node as its next node
+	if(_capacity != -1 && _size >= _capacity) {
+	    return false;
+	}
+	_front = new DLLNode<T>(x, null, _front); //making node with x as cargo and former front node as its next node
 
 	if ( _size == 0 ) 
 	    _end = _front;
@@ -62,8 +73,11 @@ public class QQKachoo<T> implements Deque<T> {
     }
 
     public boolean offerLast(T x) {
+	if(_capacity != -1 && _size >= _capacity) {
+	    return false;
+	}
         _end = new DLLNode<T>(x, _end, null); //making node with x as cargo and former end node as its previous node
-
+	
 	if ( _size == 0 ) 
 	    _front = _end;
 	else 
@@ -72,20 +86,12 @@ public class QQKachoo<T> implements Deque<T> {
 	return true;
     }
 
-    //add methods (do we need these?)
-    public void addFirst(T x) {
-
-    }
-
-    public void addLast(T x) {
-
-    }
-
     //toString method:
     public String toString() {
 	String s = "NULL<--";
 	DLLNode<T> tmp = _front;
 	int ctr = 0;
+	if (_size == 0) return "NULL<-->NULL";
 	while (ctr < _size) {
 	    s += tmp.getCargo() + "<-->";
 	    tmp = tmp.getNext();
@@ -96,22 +102,41 @@ public class QQKachoo<T> implements Deque<T> {
     
     public static void main(String[] args) {
 	QQKachoo<String> mister = new QQKachoo<String>();
+	QQKachoo<String> mrs = new QQKachoo<String>(8);
 
-	System.out.println("\nEnqueueing deque...");
+	System.out.println("\nEnqueueing deque mister...");
 
-	mister.offerLast("and");
-	mister.offerLast("there's");
-	mister.offerLast("nobody");
-	mister.offerLast("around");
+	System.out.println("Element added successfully: " + mister.offerLast("and"));
+	System.out.println("Element added successfully: " + mister.offerLast("there's"));
+	System.out.println("Element added successfully: " + mister.offerLast("nobody"));
+	System.out.println("Element added successfully: " + mister.offerLast("around"));
 
-	mister.offerFirst("forest");
-	mister.offerFirst("a");
-	mister.offerFirst("in");
-	mister.offerFirst("falling");
-	mister.offerFirst("you're");
-	mister.offerFirst("when");
+	System.out.println("Element added successfully: " + mister.offerFirst("forest"));
+	System.out.println("Element added successfully: " + mister.offerFirst("a"));
+	System.out.println("Element added successfully: " + mister.offerFirst("in"));
+	System.out.println("Element added successfully: " + mister.offerFirst("falling"));
+	System.out.println("Element added successfully: " + mister.offerFirst("you're"));
+	System.out.println("Element added successfully: " + mister.offerFirst("when"));
+	System.out.println("\nmister after enqueing: " + mister);
+
 	
-	System.out.println("\nDeque after enqueing: " + mister);
+	System.out.println("\nEnqueueing deque mrs...");
+
+	System.out.println("Element added successfully: " + mrs.offerLast("peck"));
+	System.out.println("Element added successfully: " + mrs.offerLast("of"));
+	System.out.println("Element added successfully: " + mrs.offerLast("pickled"));
+	System.out.println("Element added successfully: " + mrs.offerLast("peppers"));
+
+	System.out.println("Element added successfully: " + mrs.offerFirst("a"));
+	System.out.println("Element added successfully: " + mrs.offerFirst("picked"));
+	System.out.println("Element added successfully: " + mrs.offerFirst("Piper"));
+	System.out.println("Element added successfully: " + mrs.offerFirst("Peter"));
+	System.out.println("Element added successfully: " + mrs.offerFirst("random"));
+	System.out.println("Element added successfully: " + mrs.offerLast("thing"));
+
+	System.out.println("\nmrs after enqueing: " + mrs);
+
+	
 	   
 	while (mister._size > 0) {
 	    int firstOrLast = (int)(Math.random() * 2); //determines whether to remove element at front or end of deque
